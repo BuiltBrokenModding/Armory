c4e439ec4206145ba5059a2651402764b6e19c73
Green_Cafe <worldbrodcast@gmail.com>
2015-08-19 16:52:54 -0600

    Medieval Swords Added
    All Medieval swords have been added TEXTURELESS. Including tiered
    materials for each weapon

---
+++ src/main/java/com/builtbroken/armory/content/mediveal/ItemBronBroadSword.java
@@ -0,0 +1,14 @@
+package com.builtbroken.armory.content.mediveal;
+
+import net.minecraft.item.ItemSword;
+
+/**
+ * Created by Spencer on 8/19/2015.
+ */
+public class ItemBronBroadSword extends ItemSword{
+
+    protected ItemBronBroadSword(ToolMaterial material) {
+        super(material);
+    }
+
+}
---
+++ src/main/java/com/builtbroken/armory/content/mediveal/ItemSteelLongSword.java
@@ -0,0 +1,14 @@
+package com.builtbroken.armory.content.mediveal;
+
+import net.minecraft.item.ItemSword;
+
+/**
+ * Created by Spencer on 8/19/2015.
+ */
+public class ItemSteelLongSword extends ItemSword{
+
+    protected ItemSteelLongSword(ToolMaterial material) {
+        super(material);
+    }
+
+}

bc026a00b620823c6939add8c5482cb14e8bd713
Robert Seifert <rseifert.phone@gmail.com>
2016-11-18 05:31:11 -0500

    Added left click handling to gun

--- src/main/java/com/builtbroken/armory/content/items/ItemGun.java
+++ src/main/java/com/builtbroken/armory/content/items/ItemGun.java
@@ -3,25 +3,30 @@ package com.builtbroken.armory.content.items;
 import com.builtbroken.armory.Armory;
 import com.builtbroken.armory.data.ranged.GunData;
 import com.builtbroken.jlib.type.Pair;
+import com.builtbroken.mc.api.items.IMouseButtonHandler;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.World;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 11/16/2016.
  */
-public class ItemGun extends ItemWeapon
+public class ItemGun extends ItemWeapon implements IMouseButtonHandler
 {
     /** Map of weapons to meta values for fast access */
     public static final HashMap<Integer, GunData> metaToGun = new HashMap();
     /** Cache of the last weapon the entity has out */
     public static final HashMap<Entity, Pair<GunData, ItemStack>> gunCache = new HashMap();
+    /** Who has the left click held down */
+    public static final List<EntityPlayer> leftClickHeld = new ArrayList();
     //TODO handle what type of gun
     //TODO handle damage to weapon
     //TODO handle damage to weapon parts
@@ -34,19 +39,35 @@ public class ItemGun extends ItemWeapon
     public ItemGun()
     {
         this.setUnlocalizedName(Armory.PREFIX + "gun");
+        this.setCreativeTab(CreativeTabs.tabCombat);
         this.setHasSubtypes(true);
+    }
+
+    @Override
+    public void mouseClick(ItemStack stack, EntityPlayer player, int button, boolean state)
+    {
+        if (!player.worldObj.isRemote)
+        {
+            if (button == 0)
+            {
+                if (state)
+                {
+                    if (!leftClickHeld.contains(player))
+                    {
+                        leftClickHeld.add(player);
+                    }
+                }
+                else
+                {
+                    leftClickHeld.remove(player);
+                }
+            }
+        }
     }
 
     @Override
     public boolean onEntitySwing(EntityLivingBase entityLiving, ItemStack stack)
     {
-        if (!entityLiving.worldObj.isRemote)
-        {
-            if (entityLiving instanceof EntityPlayer)
-            {
-                ((EntityPlayer) entityLiving).addChatComponentMessage(new ChatComponentText("Bang!!!"));
-            }
-        }
         return true;
     }
 

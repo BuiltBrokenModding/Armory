f035f4aaa2fd357b9500ddc0c3869b40f35a0d17
Robert WC Seifert <rseifert.phone@gmail.com>
2017-10-24 20:42:30 -0400

    Changed player weapon user to limit searches to main inventory
    This is in attempt to fix issues where reloading will disconnect a user from a server depending on mods installed. Unknown which mod is causing the issue but this might decrease chance of errors.

--- src/main/java/com/builtbroken/armory/data/user/WeaponUserPlayer.java
+++ src/main/java/com/builtbroken/armory/data/user/WeaponUserPlayer.java
@@ -4,14 +4,14 @@ import com.builtbroken.armory.data.ranged.GunInstance;
 import com.builtbroken.mc.core.Engine;
 import com.builtbroken.mc.imp.transform.vector.Pos;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 
 /**
  * @see <a href="https://github.com/BuiltBrokenModding/VoltzEngine/blob/development/license.md">License</a> for what you can and can't do with the code.
  * Created by Dark(DarkGuardsman, Robert) on 4/11/2017.
  */
-public class WeaponUserPlayer extends WeaponUserEntity<EntityPlayer>
+public class WeaponUserPlayer extends WeaponUserEntity<EntityPlayer> implements IInventory
 {
     public WeaponUserPlayer(EntityPlayer entity)
     {
@@ -31,9 +31,9 @@ public class WeaponUserPlayer extends WeaponUserEntity<EntityPlayer>
 
 
     @Override
-    public InventoryPlayer getInventory()
+    public IInventory getInventory()
     {
-        return entity.inventory;
+        return this;
     }
 
     @Override
@@ -48,7 +48,7 @@ public class WeaponUserPlayer extends WeaponUserEntity<EntityPlayer>
         ItemStack stack = entity.getHeldItem();
         if (stack != null && stack.isItemEqual(updated))
         {
-            getInventory().setInventorySlotContents(getInventory().currentItem, updated);
+            getInventory().setInventorySlotContents(entity.inventory.currentItem, updated);
             entity.inventoryContainer.detectAndSendChanges();
 
             //Debug
@@ -57,5 +57,116 @@ public class WeaponUserPlayer extends WeaponUserEntity<EntityPlayer>
                 Engine.logger().info("Updated gun stack: " + name);
             }
         }
+    }
+
+    @Override
+    public int getSizeInventory()
+    {
+        return entity.inventory.mainInventory.length;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int slot)
+    {
+        if (slot >= 0 && slot < getSizeInventory())
+        {
+            return entity.inventory.mainInventory[slot];
+        }
+        return null;
+    }
+
+    @Override
+    public ItemStack decrStackSize(int slot, int a)
+    {
+        if (getStackInSlot(slot) != null)
+        {
+            ItemStack itemstack;
+
+            if (getStackInSlot(slot).stackSize <= a)
+            {
+                itemstack = getStackInSlot(slot);
+                setInventorySlotContents(slot, null);
+                return itemstack;
+            }
+            else
+            {
+                itemstack = getStackInSlot(slot).splitStack(a);
+
+                if (getStackInSlot(slot).stackSize == 0)
+                {
+                    setInventorySlotContents(slot, null);
+                }
+
+                return itemstack;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int slot)
+    {
+        return getStackInSlot(slot);
+    }
+
+    @Override
+    public void setInventorySlotContents(int slot, ItemStack stack)
+    {
+        if (slot >= 0 && slot < getSizeInventory())
+        {
+            entity.inventory.setInventorySlotContents(slot, stack);
+        }
+    }
+
+    @Override
+    public String getInventoryName()
+    {
+        return "Player Inventory";
+    }
+
+    @Override
+    public boolean hasCustomInventoryName()
+    {
+        return false;
+    }
+
+    @Override
+    public int getInventoryStackLimit()
+    {
+        return entity.inventory.getInventoryStackLimit();
+    }
+
+    @Override
+    public void markDirty()
+    {
+        entity.inventory.markDirty();
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer p_70300_1_)
+    {
+        return entity.inventory.isUseableByPlayer(p_70300_1_);
+    }
+
+    @Override
+    public void openInventory()
+    {
+        entity.inventory.openInventory();
+    }
+
+    @Override
+    public void closeInventory()
+    {
+        entity.inventory.closeInventory();
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int slot, ItemStack item)
+    {
+        if (slot >= 0 && slot < getSizeInventory())
+        {
+            return entity.inventory.isItemValidForSlot(slot, item);
+        }
+        return false;
     }
 }
